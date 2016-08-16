e6c3c79c4f60bb12502a5cc7e428042909a99cec
mssalvador <michael.svanholm@gmail.com>
2016-08-16 11:29:19 +0200

    Add files via upload
    The partioner is uploaded

---
+++ createCdataSample.py
@@ -0,0 +1,37 @@
+'''
+Created on Jun 20, 2016
+
+@author: svanhmic
+'''
+
+from pyspark import SparkContext
+from pyspark.sql import SQLContext,Row,DataFrameWriter,DataFrameReader
+from pyspark.rdd import RDD
+
+
+sc = SparkContext("local[8]","regnData")
+sqlContext = SQLContext(sc)
+fileStr = "/home/svanhmic/workspace/Python/Erhvervs/data/cdata"
+
+
+dataF = sqlContext.read.json(fileStr+"/cdata-permanent.json")
+virksomhedsData =  dataF.filter(dataF._type == 'virksomhed').select(dataF['_source']["Vrvirksomhed"].alias("virksomhed"))
+#virksomhedsData.printSchema()
+#print dataF.count()
+#types = dataF.select(dataF._type).distinct().collect()
+#virkNumb = dataF.filter(dataF._type == 'virksomhed').select('_source.Vrvirksomhed.cvrNummer').persist()
+#print virkNumb.count()
+#print virkNumb.distinct().count()
+#print virkNumb.take(4)
+#for r in types:
+#    smallSet = dataF.filter(dataF._type == str(r["_type"])).sample(False,0.01,42) # collect small sample 
+#    smallSet.write.json(fileStr+"/cvr1p_"+str(r["_type"])+".json",mode="overwrite")
+
+#smallerset = (virksomhedsData
+#              .select(virksomhedsData.virksomhed.cvrNummer.alias("cvr"),
+#                      virksomhedsData.virksomhed.virksomhedsstatus.status.alias("status"),
+#                      virksomhedsData.virksomhed.virksomhedsstatus.periode.gyldigFra.alias("gyldigFra"),
+#                      virksomhedsData.virksomhed.virksomhedsstatus.periode.gyldigTil.alias("gyldigTil"))
+#              )
+#smallerset.write.json(fileStr+"/AlleDatavirksomheder.json",mode="overwrite")
+virksomhedsData.write.json(fileStr+"/AlleDatavirksomheder.json",mode="overwrite")
