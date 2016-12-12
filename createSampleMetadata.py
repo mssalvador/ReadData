5f16755c2750e383696aaff748b65d5687cf61cd
Michael Svanholm <mssalvador@users.noreply.github.com>
2016-12-12 09:38:10 +0100

    Add files via upload
    Made changes enable hadoop

--- createSampleMetadata.py
+++ createSampleMetadata.py
@@ -5,39 +5,30 @@ Created on Aug 24, 2016
 '''
 import numpy as np
 import sys
-import itertools
-from string import lower
-reload(sys)
-sys.setdefaultencoding('utf-8')
 
 from pyspark import SQLContext
 from pyspark import SparkContext
-from pyspark.sql import DataFrameWriter
-from pyspark.sql.types import StringType, ArrayType, StructField, StructType,\
-    IntegerType,FloatType
-from pyspark.sql.functions import explode, udf,coalesce,split,\
-    mean,log
-from pyspark.mllib.linalg import SparseVector, VectorUDT, Vectors
-from pyspark.ml.feature import VectorAssembler, StandardScaler
+from pyspark.sql.types import StringType, IntegerType
+from pyspark.sql.functions import  udf, mean
 from re import findall, sub
 
 
-fileStr = "/home/svanhmic/workspace/Python/Erhvervs/data/cdata"
+fileStr = ""
 virksomheder = "/c1p_virksomhed.json"
 alleVirksomheder = "/AlleDatavirksomheder.json"
 produktionsenhed = "/c1p_produktionsenhed.json"
 deltager = "/c1p_deltager.json"
 onePercent = "/c1p.json"
 
-sc = SparkContext("local[8]","clustermodel")
+sc = SparkContext(appName="clustermodel")
 sqlContext = SQLContext(sc)
 
 
 def extractNumbers(col):
     
     try:
-        if type(col) is not unicode: 
-            ucol = unicode(col)
+        if not isinstance(col, str): 
+            ucol = str(col)
         else:
             ucol = col
         nums = findall('\d+',ucol)
@@ -101,63 +92,70 @@ def getIndex(rdd):
         r.append(i)
     return r
 
-virkData = sqlContext.read.format("json").load(fileStr+alleVirksomheder) # loads the subsample of virksomheder  alleVirksomheder
-virkDataTemp = virkData.select(virkData["virksomhed"]["virksomhedMetadata"].alias("metadata")
-                               ,virkData["virksomhed"]["virksomhedsstatus"].alias("virksomhedsstatus")
-                               ,virkData["virksomhed"]["brancheAnsvarskode"].alias("brancheAnsvarskode")
-                               ,virkData["virksomhed"]["reklamebeskyttet"].alias("reklamebeskyttet")
-                               ,virkData["virksomhed"]["cvrnummer"].alias("cvrnummer"))
-
-
-virkDataDf = virkDataTemp.select( virkDataTemp["metadata"]["antalPenheder"].alias("nPenheder")
-                                 ,virkDataTemp["metadata"]["nyesteAarsbeskaeftigelse"]["intervalKodeAntalAnsatte"].alias("aarsbeskfansat")
-                                 #,virkDataTemp["metadata"]["nyesteBibranche1"]["branchekode"].alias("branchekode1")   
-                                 #,virkDataTemp["metadata"]["nyesteBibranche1"]["branchetekst"].alias("branchetekst1")
-                                 #,virkDataTemp["metadata"]["nyesteBibranche2"]["branchekode"].alias("branchekode2")
-                                 #,virkDataTemp["metadata"]["nyesteBibranche2"]["branchetekst"].alias("branchetekst2")
-                                 #,virkDataTemp["metadata"]["nyesteBibranche3"]["branchekode"].alias("branchekode3")
-                                 #,virkDataTemp["metadata"]["nyesteBibranche3"]["branchetekst"].alias("branchetekst3")                                     
-                                 #,virkDataTemp["metadata"]["nyesteHovedbranche"]["branchekode"].alias("hovedbranchekode")
-                                 #,virkDataTemp["metadata"]["nyesteStatus"]["statuskode"].alias("statuskode")
-                                 ,virkDataTemp["metadata"]["nyesteVirksomhedsform"]["langBeskrivelse"].alias("langBeskrivelse")
-                                 ,virkDataTemp["metadata"]["nyesteVirksomhedsform"]["virksomhedsformkode"].alias("virksomhedsformkode")
-                                 ,virkDataTemp["metadata"]["sammensatStatus"].alias("sammensatStatus")
-                                 ,virkDataTemp["metadata"]["stiftelsesDato"].alias("stiftelsesDato")
-                                 ,virkDataTemp["cvrnummer"]
-                                 ,virkDataTemp["virksomhedsstatus"]
-                                 ,virkDataTemp["brancheAnsvarskode"]
-                                 ,virkDataTemp["reklamebeskyttet"].cast('integer').alias("reklamebeskyttet")
-                                 )
-#print virkDataDf.dtypes
-#virkDataDf.show(truncate=False)
-
-IndexedVirkDf = (virkDataDf.select(virkDataDf["cvrnummer"]
-                                   ,virkDataDf["nPenheder"]
-                                   ,extractNumberUDF(virkDataDf["aarsbeskfansat"] ).alias("antalAnsatte")
-                                   #,getStrings(virkDataDf["branchekode1"]).alias("branchekode1")
-                                   #,getStrings(virkDataDf["branchekode2"]).alias("branchekode2")
-                                   #,getStrings(virkDataDf["branchekode3"]).alias("branchekode3")
-                                   #,getStrings(virkDataDf["hovedbranchekode"]).alias("hovedbranchekode")
-                                   #,getStrings(virkDataDf["statuskode"]).alias("statuskode")
-                                   ,getStrings(virkDataDf["langBeskrivelse"]).alias("virksomhedsBeskrivelse")
-                                   ,getStrings(virkDataDf["brancheAnsvarskode"]).alias("brancheAnsvarskode")
-                                   ,extractStatusUDF(virkDataDf["sammensatStatus"]).alias("sammensatStatus")
-                                   #,virkDataDf["virksomhedsformkode"]
-                                   #,virkDataDf["stiftelsesDato"]
-                                   ,extractYearfromVirkUDF(virkDataDf["stiftelsesDato"]).alias("stiftelsesAar")                                   
-                                   ,extractVirkStatusUDF(virkDataDf["virksomhedsstatus"]).alias("virksomhedsstatus")
-                                   ,virkDataDf["reklamebeskyttet"]
-                                   )).rdd.zipWithIndex().map(lambda x: getIndex(x))
-                                   
-cleanedVirkDf = sqlContext.createDataFrame(IndexedVirkDf,["Index","cvrnummer","nPenheder","antalAnsatte"#,"branchekode1","branchekode2","branchekode3","hovedbranchekode"
-                                                          #,"statuskode"
-                                                          ,"virksomhedsBeskrivelse"
-                                                          ,"brancheAnsvarskode","sammensatStatus"
-                                                          #,"virksomhedsformkode"
-                                                          ,"stiftelsesAar","virksomhedsstatus","reklamebeskyttet"])
-
-#virkData.printSchema()
-
-#data is pivoted doing the "bag of words" and afterwards doing an imputer mean which substitutes a mean value for a column in an null value element of that col.
-
-cleanedVirkDf.write.json(fileStr+"/virksomhedersMetadata.json",mode="overwrite")
+if __name__ == '__main__':
+    
+    if len(sys.argv) == 0:
+        fileStr = "/home/svanhmic/workspace/Python/Erhvervs/data/cdata"
+    else:
+        fileStr = sys.argv[1]
+    
+    virkData = sqlContext.read.format("json").load(fileStr+alleVirksomheder) # loads the subsample of virksomheder  alleVirksomheder
+    virkDataTemp = virkData.select(virkData["virksomhed"]["virksomhedMetadata"].alias("metadata")
+                                   ,virkData["virksomhed"]["virksomhedsstatus"].alias("virksomhedsstatus")
+                                   ,virkData["virksomhed"]["brancheAnsvarskode"].alias("brancheAnsvarskode")
+                                   ,virkData["virksomhed"]["reklamebeskyttet"].alias("reklamebeskyttet")
+                                   ,virkData["virksomhed"]["cvrnummer"].alias("cvrnummer"))
+    
+    
+    virkDataDf = virkDataTemp.select( virkDataTemp["metadata"]["antalPenheder"].alias("nPenheder")
+                                     ,virkDataTemp["metadata"]["nyesteAarsbeskaeftigelse"]["intervalKodeAntalAnsatte"].alias("aarsbeskfansat")
+                                     #,virkDataTemp["metadata"]["nyesteBibranche1"]["branchekode"].alias("branchekode1")   
+                                     #,virkDataTemp["metadata"]["nyesteBibranche1"]["branchetekst"].alias("branchetekst1")
+                                     #,virkDataTemp["metadata"]["nyesteBibranche2"]["branchekode"].alias("branchekode2")
+                                     #,virkDataTemp["metadata"]["nyesteBibranche2"]["branchetekst"].alias("branchetekst2")
+                                     #,virkDataTemp["metadata"]["nyesteBibranche3"]["branchekode"].alias("branchekode3")
+                                     #,virkDataTemp["metadata"]["nyesteBibranche3"]["branchetekst"].alias("branchetekst3")                                     
+                                     #,virkDataTemp["metadata"]["nyesteHovedbranche"]["branchekode"].alias("hovedbranchekode")
+                                     #,virkDataTemp["metadata"]["nyesteStatus"]["statuskode"].alias("statuskode")
+                                     ,virkDataTemp["metadata"]["nyesteVirksomhedsform"]["langBeskrivelse"].alias("langBeskrivelse")
+                                     ,virkDataTemp["metadata"]["nyesteVirksomhedsform"]["virksomhedsformkode"].alias("virksomhedsformkode")
+                                     ,virkDataTemp["metadata"]["sammensatStatus"].alias("sammensatStatus")
+                                     ,virkDataTemp["metadata"]["stiftelsesDato"].alias("stiftelsesDato")
+                                     ,virkDataTemp["cvrnummer"]
+                                     ,virkDataTemp["virksomhedsstatus"]
+                                     ,virkDataTemp["brancheAnsvarskode"]
+                                     ,virkDataTemp["reklamebeskyttet"].cast('integer').alias("reklamebeskyttet")
+                                     )
+    #print virkDataDf.dtypes
+    #virkDataDf.show(truncate=False)
+    
+    IndexedVirkDf = (virkDataDf.select(virkDataDf["cvrnummer"]
+                                       ,virkDataDf["nPenheder"]
+                                       ,extractNumberUDF(virkDataDf["aarsbeskfansat"] ).alias("antalAnsatte")
+                                       #,getStrings(virkDataDf["branchekode1"]).alias("branchekode1")
+                                       #,getStrings(virkDataDf["branchekode2"]).alias("branchekode2")
+                                       #,getStrings(virkDataDf["branchekode3"]).alias("branchekode3")
+                                       #,getStrings(virkDataDf["hovedbranchekode"]).alias("hovedbranchekode")
+                                       #,getStrings(virkDataDf["statuskode"]).alias("statuskode")
+                                       ,getStrings(virkDataDf["langBeskrivelse"]).alias("virksomhedsBeskrivelse")
+                                       ,getStrings(virkDataDf["brancheAnsvarskode"]).alias("brancheAnsvarskode")
+                                       ,extractStatusUDF(virkDataDf["sammensatStatus"]).alias("sammensatStatus")
+                                       #,virkDataDf["virksomhedsformkode"]
+                                       #,virkDataDf["stiftelsesDato"]
+                                       ,extractYearfromVirkUDF(virkDataDf["stiftelsesDato"]).alias("stiftelsesAar")                                   
+                                       ,extractVirkStatusUDF(virkDataDf["virksomhedsstatus"]).alias("virksomhedsstatus")
+                                       ,virkDataDf["reklamebeskyttet"]
+                                       )).rdd.zipWithIndex().map(lambda x: getIndex(x))
+                                       
+    cleanedVirkDf = sqlContext.createDataFrame(IndexedVirkDf,["Index","cvrnummer","nPenheder","antalAnsatte"#,"branchekode1","branchekode2","branchekode3","hovedbranchekode"
+                                                              #,"statuskode"
+                                                              ,"virksomhedsBeskrivelse"
+                                                              ,"brancheAnsvarskode","sammensatStatus"
+                                                              #,"virksomhedsformkode"
+                                                              ,"stiftelsesAar","virksomhedsstatus","reklamebeskyttet"])
+    
+    #virkData.printSchema()
+    
+    #data is pivoted doing the "bag of words" and afterwards doing an imputer mean which substitutes a mean value for a column in an null value element of that col.
+    
+    cleanedVirkDf.write.json(fileStr+"/virksomhedersMetadata.json",mode="overwrite")
